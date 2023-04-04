be7853333e1d36c49d1e510223d9971ef95a3a1f
kwonjjinho <109893466+kwonjjinho@users.noreply.github.com>
2023-04-04 13:05:10 +0900

    Update get_num_square.py

--- get_num_square.py
+++ get_num_square.py
@@ -5,8 +5,8 @@ if num:
     try:
         num = int(num)
     expect Exception:
-        exit()
+        exit('ERROR: the INPUT-NUM provided ("{}")is npt ')
 else:
     num = 1
     
-print()
+print(f"::set-output name=num_squared::{num ** 2}")
